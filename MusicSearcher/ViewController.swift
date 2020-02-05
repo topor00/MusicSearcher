99ce17943747916977ecc5f64b4a13b03180cde7
TOPor <topor00@mail.ru>
2020-02-05 17:00:05 +0500

    Initial Commit

---
+++ MusicSearcher/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicSearcher
+//
+//  Created by Станислав Жук on 05.02.2020.
+//  Copyright © 2020 InsiderGroup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
